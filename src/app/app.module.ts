2149574ffca7dc0cf94606938aaaf8e8e6432c05
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-19 08:02:22 +0200

    dompipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 // Services
 import { YoutubeService } from './services/youtube.service';
 
-import { AppRoutingModule } from './app-routing.module';
+// Pipes
+import { SecureDomPipe } from './pipes/secure-dom.pipe';
+
+// Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    SecureDomPipe
   ],
   imports: [
     BrowserModule,
